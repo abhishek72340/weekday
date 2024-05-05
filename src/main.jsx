5b486fc6b4a2c924bc0af78d2fcda94c27f94e21
abhishek <abhisingh.72340@gmail.com>
2024-05-06 03:37:40 +0530

    fix module

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-import { store } from "./Redux/store/";
+import { store } from "./Redux/store.js";
 import { Provider } from "react-redux";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
